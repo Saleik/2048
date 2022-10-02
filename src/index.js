813194f8445c5c0a6ffdae79ae749dc806d632d5
kevin sarrazin <ksarrazin.dev@gmail.com>
2022-10-02 20:16:35 +0200

    [-][update/delete][generic-move-method] generic method moveCell replace right/left/up and down move methods

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import createTable from './functions/createTable.js';
 import createCells from './functions/createCells.js';
 import addRandomValue from './functions/addRandomValue.js';
-import up from './functions/actionsTypes/up.js';
-import down from './functions/actionsTypes/down.js';
 import left from './functions/actionsTypes/left.js';
 import right from './functions/actionsTypes/right.js';
 import hasWon from './functions/hasWon.js';
-import oneByOne from './functions/actionsTypes/moveCell.js';
+import cellOneByOne from './functions/actionsTypes/moveCell.js';
 
 const table = document.querySelector('#table');
 const newCells = createCells();
@@ -19,22 +17,11 @@ addRandomValue(cellsArray, 2);
 const GRID = createTable(cellsArray);
 window.addEventListener('keydown', function (e) {
   hasWon(parseInt(document.querySelector('#total').dataset.total))
-	switch (e.key) {
-		case 'ArrowUp':
-			oneByOne([...document.querySelectorAll('.active')], e.key);
-			break;
-		case 'ArrowDown':
-			oneByOne([...document.querySelectorAll('.active')], e.key);
-			break;
-		case 'ArrowLeft':
-			left([...document.querySelectorAll('.active')]);
-			break;
-		case 'ArrowRight':
-			right([...document.querySelectorAll('.active')]);
-			break;
-		default:
-      alert('Wrong action! Please use arrow keyboard');
-	}
-});
+  const activesCells = [...document.querySelectorAll('.active')];
 
-//test area
+  if(e.key === 'ArrowDown' || e.key === 'ArrowRight'){
+    cellOneByOne([...document.querySelectorAll('.active')].reverse(), e.key)
+  } else{
+    cellOneByOne(activesCells, e.key)
+  }
+});
