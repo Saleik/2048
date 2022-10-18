2f96cbf38cf776c866763f80ce35dd8deaa84f21
kevin sarrazin <ksarrazin.dev@gmail.com>
2022-10-18 16:14:49 +0200

    update css & refactoring haswon function

--- src/functions/score/hasWon.js
+++ src/functions/score/hasWon.js
@@ -6,18 +6,12 @@ import checkGrid from "./checkGrid.js";
  * @returns
  */
 const hasWon = (total)=>{
-
   const gridTiles = [...document.querySelectorAll('.tile')];
   const cells = [...document.querySelectorAll('.active')];
-  if(total === 2048){
-    alert(`SCORE: ${total}\n`+ 'You Win Gratz Folks!!!🥳');
-    return location.reload();
-  }else if(total > 2048 ){
-    alert(`SCORE: ${total}\n` + 'You Loose...Too Bad😭');
-    return location.reload();
-  }else if(gridTiles.length === cells.length) {
-    return checkGrid() && alert(`SCORE: ${total}\n` + 'You Loose...Too Bad😭')
-  }
+  if(gridTiles.length === cells.length && checkGrid())
+    return alert(`SCORE: ${total}\n` + 'You Loose...Too Bad😭'), location.reload()
+  if(total >= 2048)
+    return alert(`SCORE: ${total}\n`+ total === 2048 ? 'You Win Gratz Folks!!!🥳':'You Loose...Too Bad😭'), location.reload();
 }
 
 export default hasWon;
